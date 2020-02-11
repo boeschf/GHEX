3ca3e5623ad92b618c57da4212499ec2861f5d12
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2020-02-11 16:40:05 +0100

    manual test added

--- tests/simple_domain.cpp
+++ tests/simple_domain.cpp
@@ -201,3 +201,114 @@ TEST(simple_domain, exchange)
     comm.barrier();
 
 }
+
+TEST(simple_domain, manual_exchange)
+{
+    auto context_ptr = ghex::tl::context_factory<transport,threading>::create(1, MPI_COMM_WORLD);
+    auto& context = *context_ptr;
+
+    using T = double;
+    const int s = 10;
+    const int b = 2;
+    const int num_cells = (s+2*b)*(s+2*b);
+    const std::array<int, 2> offset = {b,b};
+    const std::array<int, 2> extent = {s+2*b, s+2*b};
+    const int rank       = context.rank();
+    const int left_rank  = (context.rank()+context.size()-1)%context.size();
+    const int right_rank = (context.rank()+1)%context.size();
+
+    // make one field on the cpu
+    std::vector<T> raw_field_cpu(num_cells);
+    fill_field(s, b, rank*s*s, raw_field_cpu);
+    //print_field(s, b, raw_field_cpu);
+
+    auto comm = context.get_communicator(context.get_token());
+
+#ifdef __CUDACC__
+    const int mem_size = num_cells*sizeof(T);
+    // allocate memory on gpu
+    T* gpu_ptr;
+    GT_CUDA_CHECK(cudaMalloc((void**)&gpu_ptr, mem_size);
+    // transfer memory to the gpu
+    GT_CUDA_CHECK(cudaMemcpy(gpu_ptr, raw_field_cpu.data(), mem_size, cudaMemcpyHostToDevice));
+    using msg_buffer = ghex::tl::message_buffer<gridtools::ghex::allocator::cuda::allocator<unsigned char>>;
+#else
+    using msg_buffer = ghex::tl::message_buffer<std::allocator<unsigned char>>;
+#endif
+    // make buffers for sending
+    msg_buffer left_top_send(b*b*sizeof(T));
+    msg_buffer right_top_send(b*b*sizeof(T));
+    msg_buffer left_bottom_send(b*b*sizeof(T));
+    msg_buffer right_bottom_send(b*b*sizeof(T));
+    msg_buffer left_send(b*s*sizeof(T));
+    msg_buffer right_send(b*s*sizeof(T));
+    msg_buffer top_send(b*s*sizeof(T));
+    msg_buffer bottom_send(b*s*sizeof(T));
+    // make buffers for receiving
+    msg_buffer left_top_recv(b*b*sizeof(T));
+    msg_buffer right_top_recv(b*b*sizeof(T));
+    msg_buffer left_bottom_recv(b*b*sizeof(T));
+    msg_buffer right_bottom_recv(b*b*sizeof(T));
+    msg_buffer left_recv(b*s*sizeof(T));
+    msg_buffer right_recv(b*s*sizeof(T));
+    msg_buffer top_recv(b*s*sizeof(T));
+    msg_buffer bottom_recv(b*s*sizeof(T));
+
+    auto fut_01 = comm.recv(left_top_recv, left_rank, 0);
+    auto fut_02 = comm.send(right_bottom_send, right_rank, 0);
+
+    auto fut_03 = comm.recv(left_bottom_recv, left_rank, 1);
+    auto fut_04 = comm.send(right_top_send, right_rank, 1);
+
+    auto fut_05 = comm.recv(left_recv, left_rank, 2);
+    auto fut_06 = comm.send(right_send, right_rank, 2);
+
+    auto fut_07 = comm.recv(top_recv, rank, 3);
+    auto fut_08 = comm.send(bottom_send, rank, 3);
+
+    auto fut_09 = comm.recv(bottom_recv, rank, 4);
+    auto fut_10 = comm.send(top_send, rank, 4);
+
+    auto fut_11 = comm.recv(right_top_recv, right_rank, 5);
+    auto fut_12 = comm.send(left_bottom_send, left_rank, 5);
+
+    auto fut_13 = comm.recv(right_bottom_recv, right_rank, 6);
+    auto fut_14 = comm.send(left_top_send, left_rank, 6);
+
+    auto fut_15 = comm.recv(right_recv, right_rank, 7);
+    auto fut_16 = comm.send(left_send, left_rank, 7);
+
+    fut_01.wait();
+    fut_02.wait();
+    fut_03.wait();
+    fut_04.wait();
+    fut_05.wait();
+    fut_06.wait();
+    fut_07.wait();
+    fut_08.wait();
+    fut_09.wait();
+    fut_10.wait();
+    fut_11.wait();
+    fut_12.wait();
+    fut_13.wait();
+    fut_14.wait();
+    fut_15.wait();
+    fut_16.wait();
+    
+#ifdef __CUDACC__
+    // transfer memory back to host
+    GT_CUDA_CHECK(cudaMemcpy(raw_field_cpu.data(), gpu_ptr, mem_size, cudaMemcpyDeviceToHost));
+    // free cuda memory
+    cudaFree(gpu_ptr);
+#endif
+
+    /*check_field(s, b, left_rank*s*s, rank*s*s, right_rank*s*s, raw_field_cpu);
+    
+    for (int r=0; r<context.size(); ++r)
+    {
+        comm.barrier();
+        if (comm.rank() == r)
+            print_field(s, b, raw_field_cpu);
+    }
+    comm.barrier();*/
+}
