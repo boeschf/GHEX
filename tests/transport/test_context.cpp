310c1f17eea58e2474e46564269e893ee412047a
John Biddiscombe <biddisco@cscs.ch>
2020-03-11 23:06:03 +0100

    test changes for libfabric

--- tests/transport/test_context.cpp
+++ tests/transport/test_context.cpp
@@ -18,6 +18,9 @@
 #ifdef GHEX_TEST_USE_UCX
 #include <ghex/transport_layer/ucx/context.hpp>
 using transport = gridtools::ghex::tl::ucx_tag;
+#elif GHEX_TEST_USE_LIBFABRIC
+#include <ghex/transport_layer/libfabric/context.hpp>
+using transport = gridtools::ghex::tl::libfabric_tag;
 #else
 #include <ghex/transport_layer/mpi/context.hpp>
 using transport = gridtools::ghex::tl::mpi_tag;
@@ -30,7 +33,7 @@ using threading = threading_std;
 const std::size_t size = 1024;
 
 TEST(context, multi) {
-    const int num_threads = 4;
+    const int num_threads = 1;
     auto context_ptr_1 = gridtools::ghex::tl::context_factory<transport,threading>::create(num_threads, MPI_COMM_WORLD);
     auto& context_1 = *context_ptr_1;
     auto context_ptr_2 = gridtools::ghex::tl::context_factory<transport,threading>::create(num_threads, MPI_COMM_WORLD);
